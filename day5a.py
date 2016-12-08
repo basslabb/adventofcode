d57e0faa479c6c9f50d20c5cbd9dafb4f7a43c95
basslabb <s_naslund@hotmail.com>
2016-12-08 08:34:40 +0100

    day5a and 5b seems to have been flipped

--- day5a.py
+++ day5a.py
@@ -1,29 +1,19 @@
 from hashlib import md5
-def is_valid_password(l):
-    for i in l:
-        if i == None:
-            return False
-    return True
-
 def main(s):
-    res = [None for i in range(0,8)]
+    res = []
     i = 0
-    while is_valid_password(res) == False:
+    while len(res) < 8:
         _s = (s+str(i)).encode("utf-8")
         h = md5(_s)
         if h.hexdigest().startswith("00000"):
-            pos, val  = h.hexdigest()[5:6], h.hexdigest()[6:7]
-            if pos.isdigit():
-                pos = int(pos)
-                print(res[pos:pos+1])
-                if pos < len(res) and res[pos:pos+1] is None:
-                    print(res, pos, val)
-                    res[pos:pos+1] = val
+            _h = h.hexdigest()[5:6]
+            print(_h)
+            res.append(_h)
+            print(res)
         i += 1
     print("".join(res))
 
-
 if __name__ == '__main__':
-    s = "abc"
-    #s = "reyedfim"
+    #s = "abc"
+    s = "reyedfim"
     main(s)
