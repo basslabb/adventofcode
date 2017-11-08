f8ca86286cf7a3ea25125e1893ebc304b3cad937
basslabb <s_naslund@hotmail.com>
2017-11-08 20:16:33 +0100

    Moved 2016 challenge to its own folder

---
+++ 2016/day10a.py
@@ -0,0 +1,6 @@
+def main(l):
+    
+
+if __name__ == '__main__':
+    l = open("day10input.txt").read().split("\n")
+    main(l)
---
+++ 2016/day9a.py
@@ -0,0 +1,6 @@
+def main(l):
+    pass
+
+if __name__ == '__main__':
+    l = open("day9input.txt").read().split("\n")
+    main(l)
