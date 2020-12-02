79e38471533d028a65e2681f21d945f7785993a5
Sebastian Näslund <sebastian@mynumbers.com>
2020-12-02 11:28:54 +0100

    Solution for day 2

---
+++ 2020/day2.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+def main(l):
+    print(len([x for x in l if is_valid(*x)]))
+
+
+def is_valid(num_tuple, character, password):
+    occurences = Counter(password)
+    return occurences[character] >= num_tuple[0] and occurences[character] <= num_tuple[1]
+
+
+def parse_input(s):
+    policy, password = s.split(":")
+    num_string, character = policy.split(" ")
+    num_tuple = tuple([int(x) for x in num_string.split("-")])
+    return (num_tuple, character, password)
+
+
+if __name__ == "__main__":
+    l = [parse_input(x) for x in open("day2.input.txt").read().split("\n")]
+    # l = [parse_input(x)
+    #      for x in ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]]
+    main(l)
