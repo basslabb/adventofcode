958ca57568601547ffed6cd0a59f5ecc666bc32f
Sebastian Näslund <sebastian.karl.naslund@gmail.com>
2022-12-24 15:44:54 +0100

    Solution for day10

---
+++ 2022/day10.py
@@ -0,0 +1,174 @@
+from itertools import chain
+
+
+def get_instruction(line):
+    if "noop" in line:
+        return [0]
+    _, value = line.split()
+    return [0, int(value)]
+
+
+def main(l):
+    instructions = list(chain(*[get_instruction(x) for x in l]))
+    signal_strengths = []
+    x_register = 1
+    indexes = [20, 60, 100, 140, 180, 220]
+    for i in range(1, len(instructions)):
+        instruction = instructions[i-1]
+        signal_strengths.append(x_register*(i))
+        x_register += instruction
+
+    print(sum([signal_strengths[i-1]
+               for i in range(0, len(signal_strengths)) if i in indexes]))
+
+
+if __name__ == "__main__":
+    s = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+    #l = s.splitlines()
+    l = open("day10.input.txt").read().splitlines()
+    main(l)
